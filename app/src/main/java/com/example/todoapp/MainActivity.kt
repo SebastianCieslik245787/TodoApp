5e7baec203fb7e838f5c01ed7fe891413c15f06d
Sebastian <245787@edu.p.lodz.pl>
2025-06-21 14:11:00 +0200

    Adding Add Task View

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.todoapp
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
